5d9849b3cdbb5e272110e7c35ef5a24d822d4c9c
Christoph Schwarzer <schwarcer@web.de>
2014-01-18 20:11:40 +0100

    Paket "studienberatung" gelöscht; Scanner funktioniert

---
+++ src/Studienberatung.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author perliny
+ */
+public class Studienberatung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hast du Abitur?");
+        scanner.nextLine();
+    }
+}
